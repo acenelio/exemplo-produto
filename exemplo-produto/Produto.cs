6fd62f8d002981d6ea2693646876d0d6e9a0078d
Nelio Alves <acenelio@gmail.com>
2017-10-04 12:25:52 -0300

    Projeto criado

---
+++ exemplo-produto/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace exemplo_produto {
+    class Produto {
+
+        public string nome;
+        public double preco;
+        public int quant;
+
+        public double total() {
+            double resultado = preco * quant;
+            return resultado;
+        }
+    }
+}
---
+++ exemplo-produto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace exemplo_produto {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto();
+
+            p.nome = Console.ReadLine();
+            p.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.quant = int.Parse(Console.ReadLine());
+
+            double total = p.total();
+
+            Console.WriteLine("Valor total de " + p.nome 
+                + ": " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
